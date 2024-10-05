5bf49658b45cefbd0691c622e53f16957802b080
artemyaroshevich <artem.yaroshevich.2205@gmail.com>
2024-10-05 12:42:28 +0300

    [NF] add User class and Processor -homework six new

--- src/User.ts
+++ src/User.ts
@@ -1,16 +1,18 @@
-import { name } from 'ts-jest/dist/transformers/hoist-jest'
+import {name} from 'ts-jest/dist/transformers/hoist-jest'
 
 export class User {
-  name: string
-  surname: string
-  phone: string
-  address: string
-  consentGiven: undefined | boolean
+    name: string
+    surname: string
+    phone: string
+    address: string
+    consentGiven: undefined | boolean
+    age: number
 
-  constructor(name: string, surname: string, phone: string, address: string) {
-    this.name = name
-    this.surname = surname
-    this.phone = phone
-    this.address = address
-  }
+    constructor(name: string, surname: string, phone: string, address: string, age: number) {
+        this.name = name
+        this.surname = surname
+        this.phone = phone
+        this.address = address
+        this.age = age
+    }
 }
